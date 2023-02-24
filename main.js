45456cdc809e1d5fd5603af981de01d70d9da894
DariaRaiRai <dari.shamrai@gmail.com>
2023-02-24 12:23:09 -0500

    add smooth scroll

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 window.addEventListener("scroll", (e) => {
-  document.body.style.cssText = `--scrollTop: ${this.scrollY}px`;
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
   // Y because we scroll down and can make it faster or slower
   //get the number of how much we scroll down
+});
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: ".wrapper",
+  content: ".content",
 });
